47817227851b25f038a66607066284dae6d8a6ac
DouglasMachry <DouglasMachry@PC>
2014-07-06 22:59:44 -0300

    commit projeto

--- view/corpo/relatorioFinal.php
+++ view/corpo/relatorioFinal.php
@@ -1,7 +1,29 @@
 <?php
     include_once '../../ctrl/ClienteDAO.php';
     +extract($_POST);
+    if(!isset($filtro))
+        $filtro = "";
+    
+    if(!isset($grupo))
+        $grupo = "";
     $dao = new ClienteDAO();
+    $dao = $dao->gerarRelatorio($nomeFiltro, $filtro, $grupo);
     
  ?>
-
+<table class="dados">
+    <tr>
+        <td>Nome</td>
+        <td>Munic&iacute;pio</td>
+        <td>Bairro</td>
+    </tr>
+    <?php
+        while ($linha = mysql_fetch_array($dao)){
+            echo "<tr>"
+                    . "<td>".$linha['nome']."</td>"
+                    . "<td>".$linha['nomeMunicipio']."</td>"
+                    . "<td>".$linha['nomeBairro']."</td>"
+                . "</tr>";
+        }
+    
+    ?>
+</table>
